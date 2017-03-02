2c5734ff59c2566e57e163ff76059611ace103a1
rajikaimal <inbox.rajika@gmail.com>
2017-03-02 18:36:50 +0530

    Add reference implementation

---
+++ index.js
@@ -0,0 +1,20 @@
+//import 3rd party libraries
+const express = require('express');
+const app = express();
+const request = require('request');
+//import list of endpoints from microservices
+const endpoints = require('./services/endpoints');
+const port = process.env.PORT || 3000;
+
+//reference implmentation
+// app.get('/api/users', function getUsers(req, res) {
+// 	request.get('/api/users', function(error, response, body) {
+// 		if (!error && response.statusCode == 200) {
+// 			res.send(body);
+// 		}
+// 	});
+// });
+
+app.listen(port, () => {
+	console.log(`API gateway running at port ${port}`);
+});
---
+++ services/endpoints.js
@@ -0,0 +1,20 @@
+module.exports = [
+	{
+		"host": "localhost",
+		"port": "3001",
+		"path": "/api/users/",
+		"method": "GET"
+	},
+	{
+		"host": "localhost",
+		"port": "3001",
+		"path": "/api/users/:id",
+		"method": "GET"
+	},
+	{
+		"host": "localhost",
+		"port": "3001",
+		"path": "/api/users/",
+		"method": "POST"
+	}
+];
