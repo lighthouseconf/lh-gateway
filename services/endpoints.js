445b09b188b36734f8bbdc999dbd065744df784a
TRomesh <tharakaromesh@gmail.com>
2017-10-02 11:54:45 +0530

    Add endpoints for getting and adding vectors

--- services/endpoints.js
+++ services/endpoints.js
@@ -40,5 +40,17 @@ module.exports = [
 		"port": "3001",
 		"path": "/api/messages/:chatid",
 		"method": "GET"
-	}			
+	},
+	{
+		"host": "localhost",
+		"port": "3001",
+		"path": "/api/vectors/:roomid",
+		"method": "GET"
+	},
+	{
+		"host": "localhost",
+		"port": "3001",
+		"path": "/api/vectors/",
+		"method": "POST"
+	}					
 ];
