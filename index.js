7b0a95efb96096a3507c6b60b912c77071dcaf89
TRomesh <tharakaromesh@gmail.com>
2018-10-02 08:37:19 +0530

    add user implementation

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const endpoints = require('./services/endpoints');
 const port = process.env.PORT || 3000;
 
 //reference implmentation
-// app.get('/api/users', function getUsers(req, res) {
-// 	request.get('/api/users', function(error, response, body) {
-// 		if (!error && response.statusCode == 200) {
-// 			res.send(body);
-// 		}
-// 	});
-// });
+app.get('/api/users', function getUsers(req, res) {
+	request.get('/api/users', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			res.send(body);
+		}
+	});
+});
 
 app.listen(port, () => {
 	console.log(`API gateway running at port ${port}`);
